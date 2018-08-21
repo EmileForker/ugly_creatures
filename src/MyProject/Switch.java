eb433e0fbf4bc865fd1c7ed006297536d9ad7ac7
Emil Taniverdiev <temil@gett.com>
2018-08-21 12:03:12 +0300

    remove redundant methods

--- src/MyProject/Switch.java
+++ src/MyProject/Switch.java
@@ -5,19 +5,5 @@ public class Switch {
     public static final String ERROR_MESSAGE = "Невозможно посчитать письки и пуканы";
 
     public static void main(String[] args) {
-
-        System.out.println(showLigusha(-1,3));
-
-
     }
-
-    public static String showLigusha (int X, int pukan) {
-        if ((piska < 0 ) || (pukan < 0)) {
-            return ERROR_MESSAGE;
-        }
-            String displayPiska = " " + piska + " ";
-            String displayPukan = " " + pukan + " ";
-
-            return ("У лягушки писек:" + displayPiska + " и пуканов:" + displayPukan);
-        }
-    }
+}
